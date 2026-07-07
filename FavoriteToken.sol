6879e9b5111f04643c46792f434c6f62a5210f99
mandroperz <122749908+mandroperz@users.noreply.github.com>
2026-07-07 09:24:57 +0900

    Update FavoriteToken.sol

--- FavoriteToken.sol
+++ FavoriteToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-//to do 20
+//to do 21
 contract FavoriteToken {
     string public favorite_token = "btc";
     string public favorite_token2 = "eth";
